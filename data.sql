46b510551af2af2dc3782fa13ab1401af4c0f51d
adel <adelguitoun@gmail.com>
2023-10-18 21:24:13 +0100

    Implement the project requirements

--- data.sql
+++ data.sql
@@ -1,5 +1,15 @@
-/* Populate database with sample data. */
+-- Animal: His name is Agumon. He was born on Feb 3rd, 2020, and currently weighs 10.23kg. He was neutered and he has never tried to escape.
+INSERT INTO animals (id, name, date_of_birth, neutered, weight_kg)
+VALUES (1, 'Agumon', '2020-02-03', true, 10.23);
 
-INSERT INTO animals (name) VALUES ('Luna');
-INSERT INTO animals (name) VALUES ('Daisy');
-INSERT INTO animals (name) VALUES ('Charlie');
+-- Animal: Her name is Gabumon. She was born on Nov 15th, 2018, and currently weighs 8kg. She is neutered and she has tried to escape 2 times.
+INSERT INTO animals
+VALUES (2, 'Gabumon', '2018-11-15', 2, true, 8);
+
+-- Animal: His name is Pikachu. He was born on Jan 7th, 2021, and currently weighs 15.04kg. He was not neutered and he has tried to escape once.
+INSERT INTO animals
+VALUES (3, 'Pikachu', '2021-01-07', 1, false, 15.04);
+
+-- Animal: Her name is Devimon. She was born on May 12th, 2017, and currently weighs 11kg. She is neutered and she has tried to escape 5 times.
+INSERT INTO animals
+VALUES (4, 'Devimon', '2017-05-12', 5, true, 11);
